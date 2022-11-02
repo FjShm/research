da5aa45e2b69b7a97728e88c66ac953881baaf57
FjShm <glycine.puits.1@gmail.com>
2022-11-02 17:22:16 +0900

    support for mytools.h

--- crystallinity/scattering/scattering.h
+++ crystallinity/scattering/scattering.h
@@ -11,12 +11,10 @@
 #include <boost/progress.hpp>
 #include <yaml-cpp/yaml.h>
 #include <read_dump/read_dump.h>
+#include <general/mytools.h>
 
 #include <omp.h>
 
 
-std::vector<std::string> split(const std::string&, char);
-void count_number_of_rows(const std::string&, int&);
-std::vector<double> linspace(const double&, const double&, int);
 void rotationtxt2rotmatrix(std::string&, Eigen::Matrix3d&, int&);
 
