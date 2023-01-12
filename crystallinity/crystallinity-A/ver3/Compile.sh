9815362f89321895cc6f8f8d772742f6086f567b
FjShm <glycine.puits.1@gmail.com>
2023-01-12 16:42:12 +0900

    remake branch

--- crystallinity/crystallinity-A/ver3/Compile.sh
+++ crystallinity/crystallinity-A/ver3/Compile.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # please rewrite include and lib path to yours
 
-g++ -I/home/fujii/local/include/ -L/home/fujii/local/lib64 crystallinity-A.cpp -lyaml-cpp -o Crystallinity-A.o
+g++ -std=c++17 -I/home/fujii/local/include/ -L/home/fujii/local/lib64 crystallinity-A.cpp -lyaml-cpp -o Crystallinity-A.o
