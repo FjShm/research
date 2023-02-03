f5540b0f22baaba492420c9e7a19a8e6bc9c5f9a
FjShm <glycine.puits.1@gmail.com>
2023-02-03 14:05:14 +0900

    extract only needed frames first

--- crystallinity/scattering/src/in.AUTO.sh
+++ crystallinity/scattering/src/in.AUTO.sh
@@ -1,20 +1,31 @@
 #!/bin/bash -eu
 
-DUMP_PATH=../../../sample_data/erate_0.1_dt_5.u.lammpstrj
-ROTATIONTXT_PATH=../../../sample_data/rotation.txt
+# used in param.yaml
+## dump path from me
+DUMP_PATH=sample_data/erate_0.1_dt_5.u.lammpstrj
+
+## rotation.txt path from me
+ROTATIONTXT_PATH=sample_data/rotation.txt
+
 ODIR="."
+
 ASPECT=xz
 
 DUMP_FRAMES=6
+FRAME_NUMBERS=(1 3 6)
 N=49
 M=512
 
-RATIO=(0 0.5 1.0)
 
 K=2
 RESOLUTION=501
 
+
+# used in qsub.sh
 declare -A NODES=(
     ["ika1.q"]=60
     ["tuna0.q"]=120
 )
+
+# used in AUTO.sh
+HEADER=9
