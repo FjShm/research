f39ab4007e650dbc4f8e5eec2388e81cdc364117
FjShm <glycine.puits.1@gmail.com>
2023-02-26 16:19:10 +0900

    fix algorithms

--- R2_n/src/r2n.cpp
+++ R2_n/src/r2n.cpp
@@ -4,19 +4,19 @@
 int main(int argc, char* argv[]){
     YAML::Node param = YAML::LoadFile(argv[1]);
     const std::string ipath = param["input_dump_path"].as<std::string>();
-    const std::string opath = param["output_path"].as<std::string>();
+    const std::string opath = param["output_path"].as<std::string>("LOG.out");
+    const int dump_frames = param["dump_frames"].as<int>();
     int N = param["beads_per_chain"].as<int>();
     int M = param["num_chain"].as<int>();
 
     // -------------------------------
     // max loop
-    int max_loop = std::count_rows(ipath, "TIMESTEP");
-    boost::progress_display show_progress(max_loop);
+    boost::progress_display show_progress(dump_frames);
 
     // -------------------------------
 
     ReadDump::ReadDump rd(ipath);
-    Eigen::VectorXd R2_n(N+1);
+    Eigen::VectorXd R2_n(N-1);
 
     int count = 0;
     while(rd.read_1frame()){
@@ -25,31 +25,29 @@ int main(int argc, char* argv[]){
         ++count;
         ++show_progress;
     }
-    R2_n /= (double) count;
+    R2_n /= (double)count;
 
     // output
     std::ofstream out{opath, std::ios::out | std::ios::trunc};
-    for (int n = 2; n <= N; n++)
-        out << n-1 << ' ' << R2_n(n) << std::endl;
+    for (int bond = 1; bond < N; bond++)
+        out << bond << ' ' << R2_n(bond-1) << std::endl;
 }
 
 
 void compute_R2_n(ReadDump::ReadDump &rd, int N, int M, Eigen::VectorXd& R2_n){
-    Eigen::VectorXd R2_n_tmp(N+1);
+    Eigen::VectorXd R2_n_tmp(N-1);
     std::vector<Eigen::Vector3d> coordinations;
     rd.join_3columns(coordinations, "xu", "yu", "zu");
-    Eigen::Vector3d dpos;
     // N-n+1: # of pairs for each chains
-    // n-1: # of bonds
-    for (int n = 2; n <= N; n++){
+    // b: # of bonds
+    for (int b = 1; b < N; b++){
         for (int m = 0; m < M; m++){
-            for (int start_id = 0; start_id <= N-n; start_id++){
-                int end_id = start_id + n - 1;
-                dpos = coordinations[m*N + start_id] - coordinations[m*N + end_id];
-                R2_n_tmp(n) += dpos.squaredNorm();
+            for (int start_id = 0; start_id < N-b; start_id++){
+                int end_id = start_id + b;
+                R2_n_tmp(b-1) += (coordinations[m*N + start_id] - coordinations[m*N + end_id]).squaredNorm();
             }
         }
-        R2_n_tmp(n) /= (M * (N - n + 1)) * (n-1);
+        R2_n_tmp(b-1) /= (M * (N - b)) * b;
     }
     R2_n += R2_n_tmp;
 }
