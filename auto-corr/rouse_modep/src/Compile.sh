f5ecb6a0ca3e4535b323af4483da26fe2d98637a
FjShm <shoma@fujii>
2023-12-03 20:47:51 +0900

    compile.sh

--- auto-corr/rouse_modep/src/Compile.sh
+++ auto-corr/rouse_modep/src/Compile.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-# plese rewrite include and lib path to yours
+# please rewrite include and lib path to yours
 
 g++ -std=c++17 -fconcepts -I/home/fujii/local/include/ -L/home/fujii/local/lib64 rouse_modep.cpp -lyaml-cpp -o Rouse_modep.o
